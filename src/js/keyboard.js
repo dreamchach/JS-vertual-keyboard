6b367e5c06e731a973c47787ce2ba0a5991722fc
dreamcatch <zmswkfl@naver.com>
2022-12-01 15:00:31 +0900

    TITLE : Change font-family

--- src/js/keyboard.js
+++ src/js/keyboard.js
@@ -1,11 +1,13 @@
 export class Keyboard {
   #swichEl;
+  #fontSelectEl;
   constructor() {
     this.#assignElement();
     this.#addEvent();
   }
   #assignElement() {
     this.#swichEl = document.getElementById("switch");
+    this.#fontSelectEl = document.getElementById("font");
   }
   #addEvent() {
     this.#swichEl.addEventListener("change", (event) => {
@@ -14,6 +16,10 @@ export class Keyboard {
         event.target.checked ? "dark-mode" : ""
       );
       console.log(event.target.checked);
+    });
+    this.#fontSelectEl.addEventListener("change", (event) => {
+      document.body.style.fontFamily = event.target.value;
+      console.log(event.target.value);
     });
   }
 }
